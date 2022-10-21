c07a0c8afed2864ce57534d64e8d38c862829860
ae-jm <py6556@gmail.com>
2022-10-21 17:08:55 +0900

    2022-10-21 수정

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, request, redirect
 import os
-
+import dbconn as db
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
 
-@app.route('/test/<username>')
+@app.route('/test/<username>', methods=['GET'])
 def test(username):
     print(username)
-    return render_template('test_result.html', name=username)
+    return render_template('test_result.html',name=username)
 
 @app.route('/methodin')
 def methodin():
@@ -23,11 +23,11 @@ def methodin():
 @app.route('/methodout', methods=['GET', 'POST'])
 def methodout():
     if request.method == 'POST':
-        print('POST')
+        print('Post')
         data = request.form
     else:
         print('GET')
-        data = request.args
+        data=request.args
     return render_template('method.html', data1=data, data2=request.method)
 
 @app.route('/fileupload', methods=['GET', 'POST'])
@@ -35,12 +35,62 @@ def fileupload():
     if request.method == 'GET':
         return render_template('fileinput.html')
     else:
-        f = request.files['formFile']
+        f=request.files['formFile']
         path = os.path.dirname(__file__)+'/upload/'+f.filename
         print(path)
         f.save(path)
-        print('저장성공')
+        print('저장성공!!')
         return redirect('/')
 
-if __name__ == '__main__':
+@app.route('/bloglist', methods=['GET'])
+def bloglist():
+    conn= db.dbconn()
+    cursor = conn.cursor()
+    sql='''select * from blog'''
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    print(rows)
+    return render_template('bloglist.html', data = rows)
+
+@app.route('/blogform', methods=['GET', 'POST'])
+def blogform():
+    if request.method == 'GET':
+        return render_template('blogform.html')
+    else:
+        f = request.files['formFile']
+        path = os.path.dirname(__file__)+'/static/blog/img'+f.filename
+        print(path)
+        f.save(path)
+        print('저장성공')
+        print(request.form)
+        conn = db.dbconn()
+        cursor = conn.cursor()
+        sql = '''insert into blog values(?,?,?)'''
+        data = [request.form['title'], request.form['content'], '/static/blog/img'+f.filename]
+        cursor.execute(sql,data)
+        conn.commit()
+        conn.close()
+        return redirect('/bloglist')
+        
+@app.route('/blog/<int:id>')
+def blogcontent(id):
+    conn = db.dbconn()
+    cursor = conn.cursor()
+    sql = '''select * from blog where id=?'''
+    cursor.execute(sql,id)
+    rows = cursor.fetchone()
+    conn.close()
+    return render_template('blog_content.html', data=rows)
+
+@app.route('/blogdelete/<int:id>')
+def blogdelete(id):
+    conn = db.dbconn()
+    cursor = conn.cursor()
+    sql = '''delete blog where id=?'''
+    cursor.execute(sql,id)
+    conn.commit()
+    conn.close()
+    return render_template('bloglist.html')
+
+if __name__ =='__main__':
     app.run(debug=True, port=80)
